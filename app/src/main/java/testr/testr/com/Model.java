2b5f89a2d7f7374d91e6a168dfaf4509600920ee
Andrew Nitu <andrewnitu@gmail.com>
2018-05-06 00:49:15 -0400

    f

--- app/src/main/java/testr/testr/com/Model.java
+++ app/src/main/java/testr/testr/com/Model.java
@@ -121,64 +121,27 @@ class Model extends Observable {
             answer.add(new Question(q.number, "-"));
         }
     }
-
-
-    /**
-     * Helper method to make it easier to initialize all observers
-     */
+    
     public void initObservers() {
         setChanged();
         notifyObservers();
     }
 
-    /**
-     * deletes an observer from the set of observers of this object.
-     *
-     * @param o the observer to be deleted
-     */
     @Override
     public synchronized void deleteObserver(Observer o) {
         super.deleteObserver(o);
     }
 
-    /**
-     * Adds an observer to the set of observers for this object, provided
-     * that it is not the same as some observer already in the set.
-     * The order in which notifications will be delivered to multiple
-     * observers is not specified. See the class comment.     *\
-     *
-     * @param o an observer to be added.
-     * @throws NullPointerException if the parameter o is null.
-     */
     @Override
     public synchronized void addObserver(Observer o) {
         super.addObserver(o);
     }
 
-    /**
-     * Clears the observer list so that this object no longer has any observers.
-     */
     @Override
     public synchronized void deleteObservers() {
         super.deleteObservers();
     }
 
-    /**
-     * If this object has changed, as indicated by the
-     * <code>hasChanged</code> method, then notify all of its observers
-     * and then call the <code>clearChanged</code> method to
-     * indicate that this object has no longer changed.
-     * * <p>
-     * Each observer has its <code>update</code> method called with two
-     * arguments: this observable object and <code>null</code>. In other
-     * words, this method is equivalent to:     * <blockquote><tt>
-     * notifyObservers(null)
-     * </tt></blockquote>
-     *
-     * @see Observable#clearChanged()
-     * @see Observable#hasChanged()
-     * @see Observer#update(Observable, Object)
-     */
     @Override
     public void notifyObservers() {
         super.notifyObservers();
